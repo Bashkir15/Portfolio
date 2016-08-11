e124782c4a4ac1ed0f27001267127d3c0c62ee8f
Forrest Collins <Forest.d.collins@gmail.com>
2016-08-11 06:04:06 -0700

    Added the html for the home skills

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 import sidenav from './scripts/sidenav.js';
+import canvasDraw from './scripts/canvas.js';
 
 var sidenavTrigger = document.getElementById('open-sidenav');
+var canvas = document.getElementById('banner');
 
 if (sidenavTrigger) {
 	sidenavTrigger.addEventListener('click', sidenav.openNav);
+}
+
+if (canvas) {
+	canvas.addEventListener('mousemove', canvasDraw.MouseMove, false);
+	canvas.addEventListener('mouseout', canvasDraw.MouseOut, false);
 }
--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -11,7 +11,7 @@ isDrawing = false,
 canvasW,
 canvasH;
 
-this.initialize = function (canvas_id) {
+function initialize(canvas_id) {
 	reload(canvas_id);
 
 	window.onresize = function (event) {
@@ -19,7 +19,7 @@ this.initialize = function (canvas_id) {
 	}
 }
 
-var reload = function (canvas_id) {
+function reload (canvas_id) {
 	canvas = document.getElementById(canvas_id);
 
 	if (!window.HTMLCanvasElement) {
@@ -42,12 +42,103 @@ var reload = function (canvas_id) {
 	prepare();
 	setupParticles();
 	draw();
-};
+}
 
-var prepare = function() {
+function prepare() {
 	bgContext.font = "300px 'sans-serif'";
 	bgContext.fillText(keyword, (canvasW/2) - (Math.round(bgContext.measureText(keyword).width/2)), 260);
+}
+
+function setupParticles() {
+	particles = [];
+
+	var imageData,
+	image_Data,
+	pixel,
+	width = 0,
+	i = 0,
+	slide = false;
+
+	imageData = bgContext.getImageData(0,0, canvasW, canvasH);
+	image_Data = imageData.data;
+
+	for (var height = 0; height < canvasH; height += density) {
+		++i;
+		slide = ((i & 2) == 0);
+		width = 0;
+
+		if (slide == true) {
+			width += 6;
+		}
+
+		for (width; width < canvasW; width += density) {
+			pixel = image_Data[((width + (height * canvasW)) * 4) - 1];
+
+			if (pixel == 255) {
+				particles.push({
+					color: color,
+					x: width,
+					y: height
+				});
+			}
+		}
+	}
+}
+
+function draw() {
+	context.clearRect(0,0,canvasW, canvasH);
+
+	var dx,
+	dy,
+	sqrDist,
+	scale = 1;
+
+	for (var i = 0, len = particles.length; i < len; ++i) {
+		context.beginPath();
+
+		context.moveTo(x, y - height / 2);
+		context.lineTo(x + width / 2, y - height / 4);
+		context.lineTo(x + width / 2, y + height / 4);
+		context.lineTo(x, y + height / 2);
+		context.lineTo(x - width / 2, y + height / 4);
+		context.lineTo(x - width / 2, y - height / 4);
+		context.lineTo(x, y - height / 2);
+
+		context.closePath();
+		context.fill();
+	}
+}
+
+var mouse = {
+	x: 0,
+	y: 0,
+	o: false
 };
+
+function MouseMove (e) {
+	mouse.x = e.offsetX || (e.layerX - canvas.offsetLeft);
+	mouse.y = e.offsetY || (e.layerY - canvas.offsetTop);
+
+	if (!isDrawing) {
+		isDrawing = true;
+
+		var drawTimeout = setTimeout(() => {
+			draw();
+			isDrawing = false;
+		}, 60);
+	}
+}
+
+function MouseOut (e) {
+	isDrawing = false;
+	clearTimeout(drawTimeout);
+	draw();
+}
+
+module.exports = {
+	MouseMove: MouseMove,
+	MouseOut: MouseOut
+}
 
 
 
