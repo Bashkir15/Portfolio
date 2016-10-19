3430910c84cb69e66dcfa65a96db2e0cd38651cd
Forrest Collins <Forest.d.collins@gmail.com>
2016-10-19 17:55:42 -0500

    Added the stylings and responsiveness for the works page

--- server/routes/index.server.routes.js
+++ server/routes/index.server.routes.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
 	res.render('index');
 });
 
+router.get('/skills', (req, res) => {
+	res.render('./views/pages/skills');
+});
+
+router.get('/works', (req, res) => {
+	res.render('./views/pages/works');
+});
+
+router.get('/about', (req, res) => {
+	res.render('./views/pages/about');
+});
+
+
+
 module.exports = router;
