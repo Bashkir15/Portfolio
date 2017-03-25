88cca6d38edb5de3e26d0c6b38162b2173cbe3aa
Forrest Collins <Forest.d.collins@gmail.com>
2017-03-25 13:53:21 -0500

    Pushing build. Not completely ready, but I wanted to test some things on the host

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,15 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
+
 import sass from 'gulp-sass';
-import uglifycss from 'gulp-uglifycss';
-import uglify from 'gulp-uglify';
 import autoprefix from 'gulp-autoprefixer';
 import cmq from 'gulp-combine-media-queries';
+import purify from 'gulp-purifycss'
+import cleanCss from 'gulp-clean-css'
+
+import uglifycss from 'gulp-uglifycss';
+import uglify from 'gulp-uglify';
+
 import rename from 'gulp-rename';
 import notify from 'gulp-notify';
 import browserSync from 'browser-sync';
@@ -18,7 +23,6 @@ const paths = {
 		sass: './public/static/sass/main.sass',
 		sass2: '/public/static/sass/**',
 		js: './dist/main.build.js',
-		js2: './dist/other.build.js',
 		images: './public/static/images/*'
 	},
 
@@ -28,6 +32,7 @@ const paths = {
 		images: './dist/images'
 	}
 };
+
 
 
 gulp.task('browserSync', () => {
@@ -55,7 +60,6 @@ gulp.task('styles', () => {
 				this.emit('end');
 			}
 		}))
-		.pipe(sourceMaps.init())
 		.pipe(sass({
 			errLogToConsole: true,
 			outputStyle: 'compact',
@@ -65,24 +69,47 @@ gulp.task('styles', () => {
 			browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
 			cascade: true
 		}))
+		.pipe(gulp.dest(paths.prod.css))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('buildStyles', () => {
+	gulp.src(paths.prod.css)
+		.pipe(plumber({
+			errorHandler: function(err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourceMaps.init())
+		.pipe(purify(['./public/app/**/*.js', './public/**/*.ejs']))
+		.pipe(cleanCss({
+			level: 2
+		}))
 		.pipe(uglifycss({
 			maxLineLne: 80
 		}))
 		.pipe(sourceMaps.write())
 		.pipe(rename((path) => {
-			path.extname = '.min.css'
+			path.extname ='.min.css'
 		}))
 		.pipe(gulp.dest(paths.prod.css))
-		.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('scripts', () => {
-	gulp.src([paths.dev.js, paths.dev.js2])
-		.pipe(plumber())
+	gulp.src([paths.dev.js])
+		.pipe(plumber({
+			errorHandler: function(err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourceMaps.init())
 		.pipe(uglify())
 		.pipe(rename((path) => {
 			path.extname = '.min.js'
 		}))
+		.pipe(sourceMaps.write('.'))
 		.pipe(gulp.dest(paths.prod.js))
 		.pipe(browserSync.reload({stream: true}))
 });
@@ -97,6 +124,10 @@ gulp.task('default', ['browserSync', 'scripts', 'styles', 'html'], () => {
 	gulp.watch([paths.dev.sass, paths.dev.sass2], ['styles']);
 	gulp.watch(paths.dev.html, ['html']);
 	gulp.watch(paths.dev.js, ['scripts']);
+});
+
+gulp.task('build', ['browserSync', 'scripts', 'buildStyles'], () => {
+	gulp.start(['scripts', 'buildStyles']);
 });
 
 
